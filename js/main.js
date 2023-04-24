61e6a3585bfe34bcb2ba4b8b57f65484c94264c2
wonahyeon <wah10220@gmail.com>
2023-04-24 19:01:07 +0900

    SECTION
    promotion-title : promotion-content 보여짐 여부
    promotion-content : promotion-description

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 // main header btn-menu 클릭하면
-// artist-recent-activity sub-nav 나타남
-// const menuBtn = document.querySelector('header .btn-menu');
-// const navSub = document.querySelector('.artist-recent-activity .sub-nav');
+// promotion-player sub-nav 나타남
+const menuBtn = document.querySelector('header .sns-nav .btn-menu');
+const navSub = document.querySelector('.promotion-player .sub-nav');
 
-// menuBtn.addEventListener('click', function () {
-//   navSub.classList.toggle('active');
-// })
+console.log(navSub);
+
+menuBtn.addEventListener('click', function () {
+  navSub.classList.toggle('active');
+});
 
 
 // 수평 슬라이드 기능
@@ -19,3 +21,12 @@ new Swiper('.promotion-content .swiper', {
     prevEl: '.promotion-content .swiper-button-prev'
   }
 });
+
+// promotion-title 클릭하면 promotion-content 나타남
+
+const promotionToggle = document.querySelector('.promotion-title');
+const promotionContent = document.querySelector('.promotion-content');
+
+promotionToggle.addEventListener('click', function() {
+  promotionContent.classList.toggle('active');
+})
