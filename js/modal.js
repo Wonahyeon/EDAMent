2c172e6734215ddd135d01e42eab9c666556bd5f
wonahyeon <wah10220@gmail.com>
2023-04-28 16:06:47 +0900

    SECTION
    artist : 반응형, modal

--- js/modal.js
+++ js/modal.js
@@ -9,3 +9,18 @@ const modalIuClose = modalIU.querySelector('.btn-close');
 modalIuClose.addEventListener('click',function(){
   modalIU.classList.remove('active');
 });
+
+
+// 수평 슬라이드
+new Swiper('.modal .swiper',{
+  loop: true,
+  autoplay: true,
+  slidesPerView: 5,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.modal .swiper-button-next',
+    prevEl: '.modal .swiper-button-prev'
+  }
+});
+
+// TODO 반응형 slidesPerView 달리 보이게 하기
